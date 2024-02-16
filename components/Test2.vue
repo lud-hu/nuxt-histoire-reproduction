4a984825925c900e87979951f55777eeaae48a17
Ludwig H. | polarstern <ludwig.h@polarstern-energie.de>
2024-02-16 14:34:38 +0100

    weird nuxt error repro

---
+++ components/Test2.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <h1>Test</h1>
+        <NuxtIcon name="account" />
+        <div ref="animationContainer" class="lottie-animation-container" />
+        <button>jo</button>
+    </div>
+</template>
+
+<script setup lang="ts">
+import Lottie from "lottie-web";
+import { isCollecting } from 'histoire/client'
+
+const animationContainer = ref<HTMLElement>();
+onMounted(() => {
+    if (!isCollecting() && animationContainer.value) {
+        Lottie.loadAnimation({
+            container: animationContainer.value,
+            renderer: "svg",
+            loop: true,
+            autoplay: true,
+            path: "<path d='M150 0 L75 200 L225 200 Z' style='fill:none;stroke:green;stroke-width:3' />",
+        });
+    }
+});
+</script>
+<style lang="scss"></style>
--- histoire.config.ts
+++ histoire.config.ts
@@ -3,8 +3,6 @@ import { HstVue } from '@histoire/plugin-vue'
 import { HstNuxt } from '@histoire/plugin-nuxt'
 
 export default defineConfig({
-  plugins: [
-    HstVue(),
-    HstNuxt(),
-  ],
-})
+    plugins: [HstVue(), HstNuxt()],
+    viteNodeInlineDeps: [/lottie-web/],
+});
